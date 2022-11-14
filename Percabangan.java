d012d7c4d44e15740a3bc4f9919f568f44122ebe
Aziz Tri Saputro <74166387+Aztris@users.noreply.github.com>
2022-11-14 20:53:11 +0700

    Soal no 5

---
+++ Percabangan.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author azizt
+ */
+public class Percabangan {
+    public static void main(String[] args) {
+        int a = 2;
+        if(a == 1){
+            System.out.println("BENAR");
+        }else{
+            System.out.println("SALAH");
+        }
+    }
+}
